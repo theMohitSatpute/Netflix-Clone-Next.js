01fd91c44063a97849008ab45fd8163879cacb0e
Mohit <mohitfiverr007@gmail.com>
2023-06-18 10:25:01 +0530

    final-code

---
+++ src/app/contact/page.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ContactCard from "@/app/components/ContactCard";
+import styles from "./contact.module.css";
+import ContactForm from "@/app/components/ContactForm";
+
+const Contact = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        <h1>Contact Us</h1>
+        <ContactCard />
+
+        <section className={styles.contact_section}>
+          <h2>
+            We'd love to hear <span> from you </span>
+          </h2>
+
+          <ContactForm />
+        </section>
+      </div>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29814.147538425266!2d78.99583027000789!3d20.921633801368642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd49805d8b1a711%3A0x876211c663dfbc1e!2sButi%20Bori!5e0!3m2!1sen!2sin!4v1687007478913!5m2!1sen!2sin"
+        width={100}
+        height={450}
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        className={styles.mapping}
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </>
+  );
+};
+
+export default Contact;
